df74ff21846ed3c7bd50ff98a0a383deac8ca203
louisho5 <hotszfung1993@gmail.com>
2025-09-26 01:39:39 +0800

    Update windows

--- platforms/windows/runner/build.bat
+++ platforms/windows/runner/build.bat
@@ -2,12 +2,14 @@
 echo Building Windows Runner with Version Info...
 
 REM Compile the resource file
-windres -o version.syso version.rc
+windres -i version.rc -o version.syso
 
 REM Build the executable with version info
-go build -ldflags "-H=windowsgui -s -w" -o runner.exe runner.go
+set GOOS=windows
+set GOARCH=amd64
+go build -ldflags="-H=windowsgui -s -w" -o runner.exe runner.go
 
 REM Clean up
-del version.syso
+if exist version.syso del version.syso
 
 echo Build complete: runner.exe
